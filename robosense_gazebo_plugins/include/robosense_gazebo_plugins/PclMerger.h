5e7d9e247bd42df9d896bee2cf036dfcc3828be1
Roald Ong <roald.ong@gmail.com>
2022-06-08 17:30:32 +0800

    first working time synced code

--- robosense_gazebo_plugins/include/robosense_gazebo_plugins/PclMerger.h
+++ robosense_gazebo_plugins/include/robosense_gazebo_plugins/PclMerger.h
@@ -2,6 +2,8 @@
 #define PCLMERGER_H
 
 #include <iostream>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 #include <pcl_ros/point_cloud.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -10,11 +12,20 @@ class pclmerger {
 public:
   pclmerger();
 
-  void start();
+  // void start();
+
+  void callback(const sensor_msgs::PointCloud2ConstPtr &fourf,
+                const sensor_msgs::PointCloud2ConstPtr &fivef);
 
 private:
   ros::NodeHandle n;
   ros::Publisher pub;
+
+  message_filters::Subscriber<sensor_msgs::PointCloud2> foursub;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> fivesub;
+  message_filters::TimeSynchronizer<sensor_msgs::PointCloud2,
+                                    sensor_msgs::PointCloud2>
+      sync;
 
   sensor_msgs::PointCloud2 result;
   sensor_msgs::PointCloud2 five;
--- robosense_gazebo_plugins/src/pclmerger.cpp
+++ robosense_gazebo_plugins/src/pclmerger.cpp
@@ -1,41 +1,44 @@
 #include "robosense_gazebo_plugins/PclMerger.h"
 
 pclmerger::pclmerger()
-    : pub(n.advertise<sensor_msgs::PointCloud2>("final", 100))
+    : pub(n.advertise<sensor_msgs::PointCloud2>("final", 100)),
+      foursub(n, "/four", 1), fivesub(n, "/five", 1),
+      sync(foursub, fivesub, 10) {
+  sync.registerCallback(&pclmerger::callback, this);
 
-{
   std::cout << "pclmerger object has been created!" << std::endl;
 }
 
-void pclmerger::start() {
-
-  boost::shared_ptr<sensor_msgs::PointCloud2 const> SharedPtr;
-  while (ros::ok()) {
-    SharedPtr = ros::topic::waitForMessage<sensor_msgs::PointCloud2>(
-        "/five", ros::Duration(1));
-    if (SharedPtr == NULL)
-      ROS_INFO("No laser messages received");
-    else
-      five = *SharedPtr;
-    SharedPtr = ros::topic::waitForMessage<sensor_msgs::PointCloud2>(
-        "/four", ros::Duration(1));
-    if (SharedPtr == NULL)
-      ROS_INFO("No laser messages received");
-    else
-      four = *SharedPtr;
-
-    pcl::concatenatePointCloud(five, four, result);
-    result.fields[3].name = "intensity";
-    result.header.frame_id = "five";
-    pub.publish(result);
-  }
+void pclmerger::callback(const sensor_msgs::PointCloud2ConstPtr &fourf,
+                         const sensor_msgs::PointCloud2ConstPtr &fivef) {
+  std::cout << "ran\n";
 }
+// void pclmerger::start() {
+//
+//   boost::shared_ptr<sensor_msgs::PointCloud2 const> first;
+//   boost::shared_ptr<sensor_msgs::PointCloud2 const> second;
+//   while (ros::ok()) {
+//     first = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/five");
+//     if (first == NULL)
+//       ROS_INFO("No laser messages received");
+//     second = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/four");
+//     if (second == NULL)
+//       ROS_INFO("No laser messages received");
+//
+//     // pcl::concatenatePointCloud(*first, *second, result);
+//     std::cout << "ran\n";
+//     // result.fields[3].name = "intensity";
+//     // result.header.frame_id = "five";
+//     // pub.publish(result);
+//   }
+// }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pclmerger");
 
   pclmerger obj;
-  obj.start();
+  // obj.sync.registerCallback(boost::bind(&obj.callback, _1, _2));
+  // obj.start();
 
   ros::spin();
   return 0;
